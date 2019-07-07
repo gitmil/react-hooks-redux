c5445566b07ea4e0725a85203726dbe70db15186
techsithgit <hemilp@tuplecorp.com>
2019-07-07 11:30:02 +0100

    initial

---
+++ src/pages/depositPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function DepositPage() {
+  const balance = useSelector(state => state.balance);
+  const dispatch = useDispatch();
+  function onDepositHandle() {
+    dispatch({ type: "DEPOSIT", payload: 10 });
+  }
+  return (
+    <div>
+      <h1>Balance: {balance}</h1>
+      <button onClick={onDepositHandle}>Deposit</button>
+    </div>
+  );
+}
+export default DepositPage;
---
+++ src/pages/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+
+
+function HomePage(){
+    const balance = useSelector(state => state.balance);
+    return <h1>Balance: {balance}</h1>
+}
+export default HomePage;
